2fb9a97ffbf76d3d4882343898980e1967eaa607
Combatd <combatdstar@gmail.com>
2019-12-13 15:36:50 -0800

    modify while loop in Array.my_each

--- enumerables.rb
+++ enumerables.rb
@@ -1,14 +1,17 @@
 class Array
 
 
-def my_each
-    
-    array = self
+    def my_each
+        array = self
 
-    index = 0
-        
-    while(index < array.length)
+        index = 0
+            
+        while(index < array.length)
+            yield(array[index])
+            index += 1
+        end
 
+        array # return the array
     end
 
 end
